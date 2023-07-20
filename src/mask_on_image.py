2d90d671ca053e8c87969a80560c005106cbd7fa
Aysu <75265305+aysuaticioglu@users.noreply.github.com>
2023-07-20 18:29:30 +0300

    Update mask_on_image.py

--- src/mask_on_image.py
+++ src/mask_on_image.py
@@ -1,51 +1,40 @@
+import json
 import os
+import numpy as np
 import cv2
 import tqdm
-import numpy as np
-import matplotlib.pyplot as plt
-from constant import *
+from constant import JSON_DIR, MASK_DIR
 
+# Create a list which contains every file name in "jsons" folder
+json_list = os.listdir(JSON_DIR)
 
-# Create a list which contains every file name in masks folder
-mask_list = os.listdir(MASK_DIR)
-# Remove hidden files if any
-for f in mask_list:
-    if f.startswith('.'):
-        mask_list.remove(f)
+""" tqdm Example Start"""
+iterator_example = range(1000000)
+for i in tqdm.tqdm(iterator_example):
+    pass
+""" tqdm Example End"""
 
-# For every mask image
-for mask_name in tqdm.tqdm(mask_list):
-    # Name without extension
-    mask_name_without_ex = mask_name.split('.')[0]
+# For every json file
+for json_name in tqdm.tqdm(json_list):
+    json_path = os.path.join(JSON_DIR, json_name)
+    try:
+        with open(json_path, 'r', encoding='latin-1') as json_file:
+            json_data = json_file.read()
+            json_dict = json.loads(json_data)
+    except json.JSONDecodeError as e:
+        print(f"JSONDecodeError occurred in file: {json_name}")
+        print("Error:", e)
+        continue
 
-    # Access required folders
-    mask_path      = os.path.join(MASK_DIR, mask_name)
-    image_path     = os.path.join(IMAGE_DIR, mask_name_without_ex+'.jpg')
-    image_out_path = os.path.join(IMAGE_OUT_DIR, mask_name)
+    # Create an empty mask whose size is the same as the original image's size
+    mask = np.zeros((json_dict["size"]["height"], json_dict["size"]["width"]), dtype=np.uint8)
+    mask_path = os.path.join(MASK_DIR, json_name[:-9] + ".png")
 
-    # Read mask and corresponding original image
+    # For every objects
+    for obj in json_dict["objects"]:
+        # Check the objects ‘classTitle’ is ‘Freespace’ or not.
+        if obj['classTitle'] == 'Freespace':
+            mask = cv2.fillPoly(mask, np.array([obj['points']['exterior']]), color=1)
 
-    #########################################
-    # CODE
-    #########################################
-
-    # Change the color of the pixels on the original image that corresponds
-    # to the mask part and create new image
-
-    #########################################
-    # CODE
-    #########################################
-
-    # Write output image into IMAGE_OUT_DIR folder
-
-
-    #########################################
-    # CODE
-    #########################################
-
-    # Visualize created image if VISUALIZE option is chosen
-    if VISUALIZE:
-
-        #########################################
-        # CODE
-        #########################################
+    # Write mask image into MASK_DIR folder
+    cv2.imwrite(mask_path, mask.astype(np.uint8))
