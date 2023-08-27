a058df200c810a4d09de839836e4f8984864199a
Aysu <75265305+aysuaticioglu@users.noreply.github.com>
2023-08-28 01:00:56 +0300

    Update model.py

--- src/model.py
+++ src/model.py
@@ -11,27 +11,47 @@ class FoInternNet(nn.Module):
         self.input_size = input_size
         self.n_classes = n_classes
 
-        # Here, layers are given as example, please feel free to modify them. 
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=1, stride=1)
-        self.conv2 = nn.Conv2d(in_channels=64, out_channels=n_classes, kernel_size=1, stride=1)
+        # Define encoder layers
+        self.encoder_conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, padding=1)
+        # Convolutional Layer 1 (Encoder): Processes input data, extracts and highlights features.
+        self.encoder_conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1)
+        # Convolutional Layer 2 (Encoder): Further processes features, generates new feature maps.
+
+        # Define decoder layers
+        self.decoder_conv1 = nn.Conv2d(in_channels=128, out_channels=64, kernel_size=3, padding=1)
+        # Convolutional Layer 1 (Decoder): Processes encoder features, aims to reconstruct original features.
+        self.decoder_conv2 = nn.Conv2d(in_channels=64, out_channels=n_classes, kernel_size=3, padding=1)
+        # Convolutional Layer 2 (Decoder): Produces final output, including class predictions.
+
+        # Upsample layer
+        self.upsample = nn.Upsample(size=input_size, mode='bilinear', align_corners=True)
+        # Upsample Layer: Resizes the output to the desired input size.
 
     def forward(self, x):
+        # Encoder Convolutional Layer 1
+        x = self.encoder_conv1(x)
+        x = F.relu(x)
+        # ReLU Activation after Encoder Conv1
+
+        # Encoder Convolutional Layer 2
+        x = self.encoder_conv2(x)
+        x = F.relu(x)
+        # ReLU Activation after Encoder Conv2
+
+        # Decoder Convolutional Layer 1
+        x = self.decoder_conv1(x)
+        x = F.relu(x)
+        # ReLU Activation after Decoder Conv1
+
+        # Decoder Convolutional Layer 2
+        x = self.decoder_conv2(x)
+        x = F.softmax(x, dim=1)
+        # Softmax Function for Decoder Output
+
+        # Upsample
+        x = self.upsample(x)
         
-        """This function feeds input data into the model layers defined.
-        Args:
-            x : input data
-        """
-        #########################################
-        # CODE
-        #########################################
-        x=self.conv1(x)
-        x=F.relu(x)
-        x=self.conv2(x)
-        x=nn.Softmax(dim=1)(x)
-       
         return x
-
-
 
 if __name__ == '__main__':
     model = FoInternNet(input_size=(HEIGHT, WIDTH), n_classes=N_CLASS)
